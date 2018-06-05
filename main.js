a54bf77dc7a51ee0d523e292626a228028ce3297
tmoreno <tmorenobernal@yahoo.es>
2018-06-05 22:52:05 +0200

    Moved to index.js

--- main.js
+++ main.js
@@ -36,14 +36,3 @@ app.on('window-all-closed', () => {
 		app.quit();
 	}
 });
-
-/*const Tail = require("./tail");
-
-let tail = new Tail("./log-generator/error.log", 1000);
-
-tail.on('line', (line) => {
-	console.log(line);
-});
-
-tail.start();
-*/
