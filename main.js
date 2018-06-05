302178fe75a1f1c012d18dfdac33e14162fb80f1
tmoreno <tmorenobernal@yahoo.es>
2018-06-05 22:37:00 +0200

    Added electron

---
+++ main.js
@@ -0,0 +1,49 @@
+const {app, BrowserWindow} = require('electron');
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+function createWindow() {
+	mainWindow = new BrowserWindow({width: 1024, height: 768});
+	mainWindow.loadFile('index.html');
+
+	// Open the DevTools.
+	// mainWindow.webContents.openDevTools();
+
+	mainWindow.on('closed', () => {
+		// Dereference the window object, usually you would store windows
+		// in an array if your app supports multi windows, this is the time
+		// when you should delete the corresponding element.
+		mainWindow = null;
+	});
+}
+
+app.on('ready', createWindow);
+
+app.on('activate', () => {
+	// On OS X it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (mainWindow === null) {
+		createWindow();
+	}
+});
+
+app.on('window-all-closed', () => {
+	// On OS X it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
+
+/*const Tail = require("./tail");
+
+let tail = new Tail("./log-generator/error.log", 1000);
+
+tail.on('line', (line) => {
+	console.log(line);
+});
+
+tail.start();
+*/
--- src/main.js
+++
@@ -1,9 +0,0 @@
-const Tail = require("./tail");
-
-let tail = new Tail("./log-generator/error.log", 1000);
-
-tail.on('line', (line) => {
-	console.log(line);
-});
-
-tail.start();
