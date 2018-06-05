4d2450dadd8ac33c6e3989b27dc359d762611262
tmoreno <tmorenobernal@yahoo.es>
2018-06-05 23:00:44 +0200

    Append lines to page

--- app/js/index.js
+++ app/js/index.js
@@ -3,7 +3,11 @@ const Tail = require("./tail");
 let tail = new Tail("../log-generator/error.log", 1000);
 
 tail.on('line', (line) => {
-	console.log(line);
+	let p = document.createElement("p");
+	let pContent = document.createTextNode(line); 
+	p.appendChild(pContent);
+
+	document.getElementById("log-lines").appendChild(p);
 });
 
 tail.start();
