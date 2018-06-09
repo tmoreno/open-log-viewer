87260ed390ff72cf9397cca359269374f9a2b4b7
tmoreno <tmorenobernal@yahoo.es>
2018-06-09 19:21:50 +0200

    File chooser

--- app/js/index.js
+++ app/js/index.js
@@ -1,13 +1,27 @@
 const Tail = require("./tail");
 
-let tail = new Tail("../log-generator/error.log", 1000);
+document.getElementById("file-chooser").onchange = function() {
+	const selectedFilePath = this.files[0].path;
 
-tail.on('line', (line) => {
-	let p = document.createElement("p");
-	let pContent = document.createTextNode(line); 
-	p.appendChild(pContent);
+	cleanLogLines();
 
-	document.getElementById("log-lines").appendChild(p);
-});
+	let tail = new Tail(selectedFilePath, 1000);
+	
+	tail.on('line', (line) => {
+		let p = document.createElement("p");
+		let pContent = document.createTextNode(line); 
+		p.appendChild(pContent);
+	
+		document.getElementById("log-lines").appendChild(p);
+	});
+	
+	tail.start();
+};
 
-tail.start();
+function cleanLogLines() {
+	let logLines = document.getElementById("log-lines");
+
+	while(logLines.firstChild) {
+		logLines.removeChild(logLines.firstChild);
+	}
+}
