8eae58218607fb96e3219089932c6c7dcc4a0135
tmoreno <tmorenobernal@yahoo.es>
2018-07-21 17:40:11 +0200

    Changed file chooser

--- app/js/components/FileChooser.vue
+++ app/js/components/FileChooser.vue
@@ -1,13 +1,37 @@
 <template>
-    <input type="file" @change="onFileChanged"/>
+	<v-container fill-height>
+		<v-layout align-center justify-space-around column fill-height>
+			<input type="file" ref="fileChooser" class="file-chooser" @change="onFileChanged"/>
+			<img class="log-file-format" src="images/log-file-format.svg"/>
+			<v-btn large color="info" @click="openFileChooser">Seleccionar archivo</v-btn>
+		</v-layout>
+	</v-container>
 </template>
+
+<style>
+	.file-chooser {
+		position: absolute;
+		left: 0;
+		top: 0;
+		opacity: 0;
+	}
+
+	img.log-file-format {
+		width: 200px;
+		height: 200px;
+		display: block;
+	}
+</style>
 
 <script>
 	export default {
 		methods: {
 			onFileChanged(event) {
                 this.$emit('change', event);
-            }
+			},
+			openFileChooser() {
+				this.$refs.fileChooser.click();
+			}
 		}
 	}
 </script>
