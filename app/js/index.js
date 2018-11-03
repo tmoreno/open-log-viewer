ed9cb3965ff3eb13031320d0fa7c328d7af9829e
tmoreno <tmorenobernal@yahoo.es>
2018-11-03 19:02:39 +0100

    Added i18n support

--- app/js/index.js
+++ app/js/index.js
@@ -3,16 +3,29 @@ import 'material-design-icons-iconfont/dist/material-design-icons.css';
 import 'vuetify/dist/vuetify.css';
 import 'clusterize.js/clusterize.css'
 import '../css/vuetify-custom.css';
+import en from '../js/locales/en.json';
+import es from '../js/locales/es.json';
 
 import Vue from 'vue';
 import Vuetify from 'vuetify';
+import VueI18n from 'vue-i18n';
 
 import App from './App.vue';
 
 Vue.use(Vuetify);
+Vue.use(VueI18n);
+
+const i18n = new VueI18n({
+  locale: 'en',
+  fallbackLocale: 'en',
+  messages: {
+    en, es
+  }
+});
 
 new Vue({
   el: '#app',
+  i18n,
   render: h => h(App)
 });
 
