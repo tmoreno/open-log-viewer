19704f6a7e6f30a03d0bcbfa534b4c5b702aa2a1
tmorenobernal <tmorenobernal@gmail.com>
2019-08-14 22:01:12 +0200

    Add customize mode for log files

--- app/js/components/FileViewer.vue
+++ app/js/components/FileViewer.vue
@@ -84,7 +84,49 @@
 		mounted: function() {
 			window.addEventListener('resize', this.handleResize);
 
+			ace.define('ace/mode/example', function(require, exports, module) {
+
+				var oop = require("ace/lib/oop");
+				var TextMode = require("ace/mode/text").Mode;
+				var ExampleHighlightRules = require("ace/mode/example_highlight_rules").ExampleHighlightRules;
+
+				var Mode = function() {
+					this.HighlightRules = ExampleHighlightRules;
+				};
+				oop.inherits(Mode, TextMode);
+
+				(function() {
+					// Extra logic goes here. (see below)
+				}).call(Mode.prototype);
+
+				exports.Mode = Mode;
+			});
+
+			ace.define('ace/mode/example_highlight_rules', function(require, exports, module) {
+
+				var oop = require("ace/lib/oop");
+				var TextHighlightRules = require("ace/mode/text_highlight_rules").TextHighlightRules;
+
+				var ExampleHighlightRules = function() {
+
+					//this.$rules = new TextHighlightRules().getRules();
+					this.$rules = {
+  						start: [
+							{
+								token: "info", 
+								regex: "^.*INFO.*$"
+							}
+						]
+					};
+				}
+
+				oop.inherits(ExampleHighlightRules, TextHighlightRules);
+
+				exports.ExampleHighlightRules = ExampleHighlightRules;
+			});
+
 			this.viewer = ace.edit("viewer");
+			this.viewer.getSession().setMode('ace/mode/example');
 			this.viewer.setOptions({
 				readOnly: true,
 				highlightActiveLine: false,
