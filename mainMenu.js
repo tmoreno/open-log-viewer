d536ad995afe26dfd9c1026d9fdb82313cad7621
tmorenobernal <tmorenobernal@gmail.com>
2020-03-21 21:20:21 +0100

    Bug fixed on linux and mac

--- mainMenu.js
+++ mainMenu.js
@@ -30,6 +30,7 @@ const template = [
                     dialog.showMessageBox({
                         type: "info",
                         title: "About",
+                        buttons: ["OK"],
                         message: "Open Log Viewer 1.2.1",
                         detail: "Developed by Tomás Moreno Bernal\nLicensed under the GNU General Public License v3.0"
                     });
